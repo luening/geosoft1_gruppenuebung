97f3635327be8153e228d01c0232c78a7d90fd1e
luening <he-luening@t-online.de>
2022-06-21 21:36:16 +0200

    localhost zu 127.0.0.1 geändert

--- routes/show.js
+++ routes/show.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
-const url = "mongodb://localhost:27017"; // connection URL
+const url = "mongodb://127.0.0.1:27017"; // connection URL
 const client = new MongoClient(url); // mongodb client
 const dbName = "mydatabase"; // database name
 const collectionName = "pois"; // collection name
@@ -11,29 +11,21 @@ const collectionName = "pois"; // collection name
 router.get("/", function (req, res, next) {
   // connect to the mongodb database and retrieve all docs
   client.connect(function (err) {
-    //assert.equal(null, err);
+    assert.equal(null, err);
 
     console.log('Connected successfully to server');
 
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
 
-    collection.find({}).toArray(function(err, docs) 
-    {
+    collection.find({}).toArray(function (err, docs) {
       assert.equal(err, null);
       console.log('Found the following records...');
-      res.render('show', { title: 'PoIs anzeigen', data: docs});
+      res.render('show', { title: 'PoIs anzeigen', data: docs });
 
     })
 
   })
-
-    collection.find({}).toArray(function (err, docs) {
-      //assert.equal(err, null);
-      console.log("Found the following records...");
-      res.render("show", { title: "PoIs anzeigen", data: docs });
-    });
-  });
 });
 
 module.exports = router;
