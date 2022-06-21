5d02377577746b45c886c906941a09160d3f3839
luening <he-luening@t-online.de>
2022-06-21 20:15:40 +0200

    localhost zu 127.0.0.1 geändert

--- routes/delete.js
+++ routes/delete.js
@@ -7,7 +7,7 @@ const assert = require('assert');
 
 
 //MongoClient and DB
-const url = 'mongodb://localhost:27017' ;// connection URL
+const url = 'mongodb://127.0.0.1:27017' ;// connection URL
 const client = new MongoClient(url); // mongodb client
 //const { stringify } = require('querystring');
 const dbName = 'mydatabase' ;// database name
--- routes/show.js
+++ routes/show.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const url = 'mongodb://localhost:27017'; // connection URL
+const url = 'mongodb://127.0.0.1:27017'; // connection URL
 const client = new MongoClient(url); // mongodb client
 const dbName = 'mydatabase' ;// database name
 const collectionName = 'pois'; // collection name
