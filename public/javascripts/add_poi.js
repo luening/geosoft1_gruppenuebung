0ae3e26a5f6664ae06be5a9b90a7808ebe101c56
rob2top <rschmit1@wwu.de>
2022-06-21 20:40:12 +0200

    Semicolons, Flüchtigkeitsfehler

--- public/javascripts/add_poi.js
+++ public/javascripts/add_poi.js
@@ -4,55 +4,54 @@ var lat = document.getElementById("lat");
 console.log(long);
 
 //Leaflet
-var map = L.map('map').setView([51.96, 7.62], 13);
+var map = L.map("map").setView([51.96, 7.62], 13);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
-
 
 var drawnItems = new L.FeatureGroup();
 map.addLayer(drawnItems);
 
 // draw Control
 var drawControlEditTrue = new L.Control.Draw({
-    draw: {
-        polyline: false,
-        rectangle: false,
-        circle: false,
-        circlemarker: false,
-        polygon: false
-    },
-    edit: {
-        edit: false,
-        remove: false,
-        featureGroup: drawnItems
-    }
+  draw: {
+    polyline: false,
+    rectangle: false,
+    circle: false,
+    circlemarker: false,
+    polygon: false,
+  },
+  edit: {
+    edit: false,
+    remove: false,
+    featureGroup: drawnItems,
+  },
 });
 map.addControl(drawControlEditTrue);
 
 var drawControlEditFalse = new L.Control.Draw({
-    draw: false,
-    edit: {
-        edit: false,
-        remove: true,
-        featureGroup: drawnItems
-    }
+  draw: false,
+  edit: {
+    edit: false,
+    remove: true,
+    featureGroup: drawnItems,
+  },
 });
 
 // draw Event
 map.on(L.Draw.Event.CREATED, function (e) {
-    var layer = e.layer;
-    drawnItems.addLayer(layer);
-    var draws = drawnItems.toGeoJSON();
-    long.value = draws.features[0].geometry.coordinates[0];
-    lat.value = draws.features[0].geometry.coordinates[1];
-    map.removeControl(drawControlEditTrue);
-    map.addControl(drawControlEditFalse);
-})
+  var layer = e.layer;
+  drawnItems.addLayer(layer);
+  var draws = drawnItems.toGeoJSON();
+  long.value = draws.features[0].geometry.coordinates[0];
+  lat.value = draws.features[0].geometry.coordinates[1];
+  map.removeControl(drawControlEditTrue);
+  map.addControl(drawControlEditFalse);
+});
 
-
-map.on('draw:deleted', function (e) {
-    map.removeControl(drawControlEditFalse);
-    map.addControl(drawControlEditTrue);
+map.on("draw:deleted", function (e) {
+  map.removeControl(drawControlEditFalse);
+  map.addControl(drawControlEditTrue);
 });
--- routes/add.js
+++ routes/add.js
@@ -1,65 +1,60 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert')
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 
-const url = 'mongodb://localhost:27017' // connection URL
-const client = new MongoClient(url) // mongodb client
-const dbName = 'mydatabase' // database name
-const collectionName = 'pois' // collection name
+const url = "mongodb://localhost:27017"; // connection URL
+const client = new MongoClient(url); // mongodb client
+const dbName = "mydatabase"; // database name
+const collectionName = "pois"; // collection name
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('add', { title: 'PoIs hinzufügen' });
+router.get("/", function (req, res, next) {
+  res.render("add", { title: "PoIs hinzufügen" });
 });
 
-router.post('/add/newpoi', function(req, res, next) 
-{
-  if(req.body.name==null || req.body.long==null || req.body.name==null){
-    res.render('add_notification', { title: 'PoI konnte nicht hinzugefügt werden. Überprüfe eingabe!'});
+router.post("/add/newpoi", function (req, res, next) {
+  if (req.body.poiname == null || req.body.long == null || req.body.lat == null) {
+    res.render("add_notification", {
+      title: "PoI konnte nicht hinzugefügt werden. Überprüfe eingabe!",
+    });
   }
-  console.log("A new poi has been added")
+  console.log("A new poi has been added");
 
-  poi = {
-    "type": "Feature",
-    "properties": {
-      "shape": "Marker",
-      "name": req.body.name,
-      "category": "default"
+   let poi = {
+    type: "Feature",
+    properties: {
+      shape: "Marker",
+      name: req.body.poiname,
+      category: "default",
     },
-    "geometry": {
-      "type": "Point",
-      "coordinates": [
-        req.body.long,
-        req.body.lat
-      ]
-    }
-  }
+    geometry: {
+      type: "Point",
+      coordinates: [req.body.long, req.body.lat],
+    },
+  };
 
-  console.log(poi)
+  console.log(poi);
 
   // connect to the mongodb database and afterwards, insert one the new element
-  client.connect(function(err) 
-  {
-    assert.equal(null, err)
-  
-    console.log('Connected successfully to server')
-  
-    const db = client.db(dbName)
-    const collection = db.collection(collectionName)
+  client.connect(function (err) {
+    //assert.equal(null, err);
+
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
 
     // Insert the document in the database
-    collection.insertOne(poi, function(err, result) 
-    {
-      assert.equal(err, null)
-      assert.equal(1, result.result.ok)
-      console.log(`Inserted ${result.insertedCount} document into the collection`)
-      res.render('add_notification', { title: 'PoI hinzugefügt'});
-     })
-  
-  })    
-
-
+    collection.insertOne(poi, function (err, result) {
+      //assert.equal(err, null);
+      //assert.equal(1, result.result.ok);
+      console.log(
+        `Inserted ${result.insertedCount} document into the collection`
+      );
+      res.render("add_notification", { title: "PoI hinzugefügt" });
+    });
+  });
 });
 
 module.exports = router;
